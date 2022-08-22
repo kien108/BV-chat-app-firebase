d11259cb33d94e23d905bc1b5aa8a708c59be431
kien108 <trungkien2062001@gmail.com>
2022-08-22 16:59:22 +0700

    ver2

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,26 +1,11 @@
 import { MenuProps } from "antd";
-import { motion } from "framer-motion";
 import React from "react";
 import StyledMenu from "./styles";
 
 interface Props extends MenuProps {}
 
 const Menu: React.FC<Props> = (props) => {
-   return (
-      <motion.div
-         initial={{
-            x: "-100%",
-         }}
-         animate={{
-            x: 0,
-         }}
-         transition={{
-            duration: 0.8,
-         }}
-      >
-         <StyledMenu {...props} />
-      </motion.div>
-   );
+   return <StyledMenu {...props} />;
 };
 
 export default Menu;
--- src/layout/pages/LayoutNone.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-
-const LayoutNone = () => {
-   return <Outlet />;
-};
-
-export default LayoutNone;
